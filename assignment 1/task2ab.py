3261242396deab3195d3384d0c9012c19fd48f7a
kristofferkl <kristofferkl2903@gmail.com>
2023-10-24 11:28:27 +0200

    inversion complete

--- assignment 1/task2ab.py
+++ assignment 1/task2ab.py
@@ -1,7 +1,9 @@
 import matplotlib.pyplot as plt
 import pathlib
+import numpy as np
 import PIL
-from utils import read_im, save_im
+from utils import read_im, save_im, normalize
+
 output_dir = pathlib.Path("image_solutions")
 output_dir.mkdir(exist_ok=True)
 
@@ -10,13 +12,10 @@ im = read_im(pathlib.Path("images", "duck.jpeg"))
 plt.imshow(im)
 
 
-print(im.shape)
+#print(im.shape)
 def greyscale(im):
     
-    R = im[:, :, 0]
-    G = im[:, :, 1]
-    B = im[:, :, 2]
-    im = 0.212*R+0.7152*G+0.0722*B
+   
     """ Converts an RGB image to greyscale
 
     Args:
@@ -25,6 +24,10 @@ def greyscale(im):
     Returns:
         im ([type]): [np.array of shape [H, W]]
     """
+    R = im[:, :, 0]
+    G = im[:, :, 1]
+    B = im[:, :, 2]
+    im = 0.212*R+0.7152*G+0.0722*B
 
     return im
 
@@ -33,7 +36,7 @@ im_greyscale = greyscale(im)
 save_im(output_dir.joinpath("duck_greyscale.jpeg"), im_greyscale, cmap="gray")
 plt.imshow(im_greyscale, cmap="gray")
 
-
+#print(im_greyscale)
 def inverse(im):
     """ Finds the inverse of the greyscale image
 
@@ -44,10 +47,17 @@ def inverse(im):
         im ([type]): [np.array of shape [H, W]]
     """
     # YOUR CODE HERE
-    width,height = im_greyscale.size
-    for w in range(width):
-        for h in range(height):
-            px=im_greyscale.getpixel((w,h))
-            px = 255-px
-            
+    # width,height, colors = im.shape
+    # for w in range(width):
+    #     for h in range(height):
+    px = im_greyscale[:]
+
+            #px=im_greyscale.getpixel((w,h))
+            #print(px)
+    im = 255-px
+    im = normalize(im)
     return im
+
+im_invert = inverse(im)
+save_im(output_dir.joinpath("duck_invert.jpeg"), im_invert, cmap="gray")
+plt.imshow(im_invert, cmap="gray")
